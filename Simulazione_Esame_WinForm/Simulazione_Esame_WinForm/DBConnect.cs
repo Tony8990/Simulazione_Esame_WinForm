ae816eb53b8627865afa4de6362a84c0ab577fb2
Tony8990 <tony.fava@edu.itspiemonte.it>
2018-06-30 15:19:20 +0200

    Add Progect

---
+++ Simulazione_Esame_WinForm/Simulazione_Esame_WinForm/DBConnect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Simulazione_Esame_WinForm.Class
+{
+    public class DBConnect
+    {
+        public string ConnectionString { get; set; }
+        public DBConnect(string Conn)
+        {
+            ConnectionString = ConfigurationSettings.AppSettings["SQl_Connection"];
+        }
+         
+    }
+           
+}
+
+
